4ded7824e575ac49b6f6d014eb2e3e65d5da3569
Ahmedm2002 <ahmedmujtaba0159@gmail.com>
2024-08-25 13:21:02 +0500

    aligned all products --Ahmed

--- src/Components/Featured Products/AllProducts.jsx
+++ src/Components/Featured Products/AllProducts.jsx
@@ -4,52 +4,61 @@ import serum from "./Images/serum.jpg";
 import faceWash from "./Images/faceWash.jpg";
 import moisturizer from "./Images/moisturizer.jpg";
 import teaTreeOil from "./Images/teaTreeOil.jpg";
-
+import cleanser from "./Images/facialCleanser.jpg";
 function FeaturedProducts() {
   return (
     <>
-      <section class="bg-white py-12 text-gray-700 sm:py-16 lg:py-20">
-        <div class="mx-auto max-w-screen-xl px-4 sm:px-6 lg:px-8">
-          <div class="mx-auto max-w-md text-center">
-            <h2 class="font-serif text-2xl font-bold sm:text-3xl">
+      <section className="bg-white py-12 text-gray-700 sm:py-16 lg:py-8">
+        <div className="mx-auto max-w-screen-xl ">
+          <div className="mx-auto max-w-md text-center">
+            <h2 className="font-serif text-2xl font-bold sm:text-3xl">
               Our featured products
             </h2>
-            {/* <p class="mt-4 text-base text-gray-700">
+            {/* <p className="mt-4 text-base text-gray-700">
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Purus
               faucibus massa dignissim tempus.
             </p> */}
           </div>
-          <div class="mt-10 grid grid-cols-2 gap-6 lg:mt-16 lg:grid-cols-4 lg:gap-4">
-            <ProductCard
-              img={serum}
-              text="Serum"
-              price="299/-"
-              reducedPrice="149/-"
-              shortDetail="Material: Oil Package Includes: 1 x Oil"
-              rating="4.0"
-            />
-            <ProductCard
-              img={faceWash}
-              text="Face Wash"
-              price="2999/-"
-              reducedPrice="1499/-"
-              onSale={true}
-              shortDetail="Material: Oil Package Includes: 1 x Oil"
-            />
-            <ProductCard
-              img={teaTreeOil}
-              text="Tea Tree Oil, 10 Ml"
-              price="450/-"
-              reducedPrice="199/-"
-              shortDetail="Material: Oil Package Includes: 1 x Oil"
-            />
-            <ProductCard
-              img={moisturizer}
-              text="Body Moisturizer by Olivia"
-              price="999/-"
-              reducedPrice="499/-"
-              shortDetail="Material: Oil Package Includes: 1 x Oil"
-            />
+          <div className="mt-4 lg:mt-8 flex flex-row flex-wrap gap-12 justify-center">
+            <div className="w-[90%] sm:w-1/3 md:w-1/4 lg:w-1/5">
+              <ProductCard
+                img={faceWash}
+                text="Face Wash"
+                price="2999/-"
+                reducedPrice="1499/-"
+                onSale={true}
+                shortDetail="Material: Oil Package Includes: 1 x Oil"
+              />
+            </div>
+            <div className="w-[90%] sm:w-1/3 md:w-1/4 lg:w-1/5">
+              <ProductCard
+                img={teaTreeOil}
+                text="Tea Tree Oil, 10 Ml"
+                price="450/-"
+                reducedPrice="199/-"
+                shortDetail="Material: Oil Package Includes: 1 x Oil"
+              />
+            </div>
+            <div className="w-[90%] sm:w-1/3 md:w-1/4 lg:w-1/5">
+              <ProductCard
+                img={moisturizer}
+                text="Body Moisturizer by Olivia"
+                price="999/-"
+                reducedPrice="499/-"
+                shortDetail="Material: Oil Package Includes: 1 x Oil"
+                rating="5.0"
+              />
+            </div>
+            <div className="w-[90%] sm:w-1/3 md:w-1/4 lg:w-1/5">
+              <ProductCard
+                img={cleanser}
+                text="Foaming Facial Cleanser"
+                price="999/-"
+                reducedPrice="499/-"
+                shortDetail="A gentle enzyme and amino acid blend loosens makeup, oil and other impurities on the skin and in the pores"
+                rating="5.0"
+              />
+            </div>
           </div>
         </div>
       </section>
