ac640535337339759f42c17f6003776bd05d9ce5
Ahmedm2002 <ahmedmujtaba0159@gmail.com>
2024-08-26 15:19:01 +0500

    Added ScrollRestoration --Ahmed

--- src/Pages/Layout.jsx
+++ src/Pages/Layout.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Outlet } from "react-router-dom";
+import { Outlet, ScrollRestoration } from "react-router-dom";
 import Header from "../Components/Header";
 import Footer from "../Components/Footer";
 function Layout() {
@@ -7,7 +7,10 @@ function Layout() {
     <>
       <div className="min-h-screen flex flex-col">
         <Header />
-        <Outlet />
+        <ScrollRestoration />
+        <main>
+          <Outlet />
+        </main>
         <Footer />
       </div>
     </>
