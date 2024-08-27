7b2a7286c4c229bf3e847ca41e71c85711a44932
Ahmedm2002 <ahmedmujtaba0159@gmail.com>
2024-08-27 22:06:47 +0500

    Minor changes in gap b/w categories --Ahmed

--- src/Components/Categories/AllCategories.jsx
+++ src/Components/Categories/AllCategories.jsx
@@ -14,13 +14,13 @@ import multaniMitti from "./CategoryImages/multaniMitti.webp";
 function AllCategories() {
   return (
     <>
-      <section className="bg-white text-gray-700 sm:py-16 lg:py-8 py-12 ">
+      <section className="bg-white text-gray-700 sm:py-8 lg:py-8 py-4 ">
         <div className="mx-auto max-w-md text-center">
           <h2 className="font-serif text-2xl font-bold sm:text-3xl">
             Explore Categories
           </h2>
         </div>
-        <div className="flex flex-wrap flex-row mt-6 md:mt-10 justify-center items-center md:gap-6 gap-3">
+        <div className="flex flex-wrap flex-row mt-6 md:mt-10 justify-center items-center sm:gap-3 md:gap-6 gap-1">
           <Category img={sunblock} name="Sunblocks" url="sunblock" />
           <Category img={faceWash} name="Face Washs" url="faceWash" />
           <Category img={facialKit} name="Facial Kits" url="facialKit" />
