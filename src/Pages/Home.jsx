7365427f7845ef63d72e97a66dafbd0347763f87
Ahmedm2002 <ahmedmujtaba0159@gmail.com>
2024-09-01 22:56:12 +0500

    Made header sticky --Ahmed

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import FeaturedProducts from "../Components/FeaturedProducts/FeaturedProducts";
 import AllCategories from "../Components/Categories/AllCategories";
+import banner from "../../public/banner.jpg";
 function Home() {
   return (
     <>
+      {/* <img src={banner} className="w-full h-96 md:w-[97%] mx-auto" /> */}
       <AllCategories />
       <FeaturedProducts />
     </>
