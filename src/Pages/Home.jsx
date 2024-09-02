989b2d8cba3b44b989653e7f138dce6fee11dd24
Ahmedm2002 <ahmedmujtaba0159@gmail.com>
2024-09-03 00:52:06 +0500

    Added testing banner --Ahmed

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import FeaturedProducts from "../Components/FeaturedProducts/FeaturedProducts";
 import AllCategories from "../Components/Categories/AllCategories";
+import banner from "../../public/banner.png";
 function Home() {
   return (
     <>
+      <img src={banner} alt="" className="w-full sm:h-auto h-72" />
       <AllCategories />
       <FeaturedProducts />
     </>
