f79770f1c660df6f5052ce7543018dc77a5ec2fb
Ahmed Mujtaba <ahmedmujtaba0159@gmail.com>
2025-01-06 21:45:00 +0500

    deleted unnecessary files enabled whatsapp logo --Ahmed

--- src/main.jsx
+++ src/main.jsx
@@ -29,8 +29,6 @@ import BrighteningCream from "./Components/Categories/Brightening Cream/Brighten
 import Serums from "./Components/Categories/Serums/Serums.jsx";
 import FaceMasks from "./Components/Categories/Masks/FaceMasks.jsx";
 import ProductDetails from "./Components/Product Cards/ProductDetails.jsx";
-import CalculatePrice from "../CalculatePrice";
-import CalculateDiscount from "../CalculateDiscount";
 import Powders from "./Components/Categories/Powders/Powders.jsx";
 import Toners from "./Components/Categories/Toners/Toners.jsx";
 import PackOfs from "./Components/PackOfs/PackOfs.jsx";
@@ -113,14 +111,7 @@ const App = () => {
           path="/:category/:productName"
           element={<ProductDetails />}
         />
-        <Route
-          path="calculatePrice"
-          element={<CalculatePrice />}
-        />
-        <Route
-          path="calculateDiscount"
-          element={<CalculateDiscount />}
-        />
+
         <Route
           path="packofs"
           element={<PackOfs />}
@@ -137,11 +128,11 @@ const App = () => {
 
   return (
     <>
-      {/* <img
+      <img
         src={whatsapp}
         onClick={() => chatOnWhatsapp()}
         className="w-12 h-12 fixed bottom-10 right-5 z-50 hover:cursor-pointer"
-      /> */}
+      />
       <RouterProvider router={router} />;
     </>
   );
